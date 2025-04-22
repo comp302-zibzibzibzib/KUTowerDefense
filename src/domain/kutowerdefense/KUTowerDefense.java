70466655cdf4b3897fad3e5986a74070f6766cf0
BedirhanTheDude <bedirhan190734gfb@gmail.com>
2025-04-23 00:26:01 +0300

    Created method bases

--- src/domain/kutowerdefense/KUTowerDefense.java
+++ src/domain/kutowerdefense/KUTowerDefense.java
@@ -1,5 +1,25 @@
 package domain.kutowerdefense;
 
-public class KUTowerDefense {
+import domain.map.Map;
+import domain.map.MapEditor;
 
+public class KUTowerDefense {
+	
+	private KUTowerDefense() {} // Can't create instance of KUTowerDefense
+	
+	public static void newGame(Map map) {
+		
+	}
+	
+	public static void enterMapEditor() {
+		MapEditor editor = new MapEditor(); // idk
+	}
+	
+	public static void openOptions() {
+		
+	}
+	
+	public static void quitGame() {
+		System.exit(0);
+	}
 }
--- src/domain/kutowerdefense/Player.java
+++ src/domain/kutowerdefense/Player.java
@@ -1,5 +1,44 @@
 package domain.kutowerdefense;
 
-public class Player {
+import domain.map.Lot;
+import domain.tower.AttackType;
 
+public class Player {
+	private static final int STARTING_GOLD = 200; // ??
+	private static final int STARTING_LIVES = 20;
+	
+	private static Player instance;
+	
+	public int gold;
+	public int lives;
+	public int waveNumber;
+	
+	private Player() {
+		this.gold = STARTING_GOLD; this.lives = STARTING_LIVES; this.waveNumber = 0;
+	}
+	
+	public static void constructTower(Lot lot, AttackType attackType) {
+		
+	}
+	
+	public static void sellTower(Lot lot) {
+		
+	}
+	
+	public static Player getInstance() {
+		if (instance == null) instance = new Player();
+		return instance;
+	}
+	
+	public static void resetPlayer() {
+		if (instance == null) instance = new Player();
+		instance.gold = STARTING_GOLD;
+		instance.lives = STARTING_LIVES;
+		instance.waveNumber = 0;
+	}
+	
+	public static void takeDamage() {
+		if (instance == null) instance = new Player();
+		instance.lives -= 1;
+	}
 }
