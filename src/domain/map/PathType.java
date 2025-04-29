8252f2e945abdfaba1e1e390ad6e312d26f98efa
dasBorke <bekobylu@hotmail.com>
2025-04-29 22:43:13 +0300

    Added Pre-Built Map 2

--- src/domain/map/PathType.java
+++ src/domain/map/PathType.java
@@ -5,8 +5,8 @@ public enum PathType {
 	HORIZONTAL_MIDDLE(" HM", new boolean[] {false, false, true, true}), HORIZONTAL_END_LEFT("HEL", new boolean[] {false, false, false, true}),
 	HORIZONTAL_END_RIGHT("HER", new boolean[] {false, false, true, false}), VERTICAL_MIDDLE(" VM", new boolean[] {true, true, false, false}), 
 	VERTICAL_END_TOP("VET", new boolean[] {false, true, false, false}), VERTICAL_END_BOTTOM("VEB", new boolean[] {true, false, false, false}),
-	RIGHT(" R ", new boolean[] {true, true, false, false}), LEFT(" L ", new boolean[] {true, true, false, false}),
-	TOP(" T ", new boolean[] {false, false, true, true}), BOTTOM(" B ", new boolean[] {false, false, true, true}),
+	RIGHT(" RT", new boolean[] {true, true, false, false}), LEFT(" LT", new boolean[] {true, true, false, false}),
+	TOP(" TP", new boolean[] {false, false, true, true}), BOTTOM(" BT", new boolean[] {false, false, true, true}),
 	BOTTOMRIGHT("BTR", new boolean[] {true, false, true, false}), BOTTOMLEFT("BTL", new boolean[] {true, false, false, true}),
 	TOPLEFT("TPL", new boolean[] {false, true, false, true}), TOPRIGHT("TPR", new boolean[] {false, true, true, false});
 	
