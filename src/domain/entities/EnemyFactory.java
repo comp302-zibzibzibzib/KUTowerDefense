4c8e6e1994006ffacd799300339492acc5acc27b
BedirhanTheDude <bedirhan190734gfb@gmail.com>
2025-05-01 13:42:28 +0300

    fixed enemy factory

--- src/domain/entities/EnemyFactory.java
+++ src/domain/entities/EnemyFactory.java
@@ -3,7 +3,6 @@ package domain.entities;
 import domain.kutowerdefense.GameOptions;
 
 public class EnemyFactory {
-	private static GameOptions options = GameOptions.getInstance();
 	// idk about these values
 	private static double GOBLIN_HIT_POINTS = 30;
 	private static double KNIGHT_HIT_POINTS = 50;
@@ -11,10 +10,10 @@ public class EnemyFactory {
 	private static double KNIGHT_ARROW_REDUCTION = 10;
 	
 	public static Goblin createGoblin() {
-		return new Goblin(GOBLIN_HIT_POINTS, options.getEnemySpeed(), null, GOBLIN_SPELL_REDUCTION);
+		return new Goblin(GOBLIN_HIT_POINTS, GameOptions.getInstance().getEnemySpeed(), null, GOBLIN_SPELL_REDUCTION);
 	}
 	
 	public static Knight createKnight() {
-		return new Knight(KNIGHT_HIT_POINTS, options.getEnemySpeed(), null, KNIGHT_ARROW_REDUCTION);
+		return new Knight(KNIGHT_HIT_POINTS, GameOptions.getInstance().getEnemySpeed(), null, KNIGHT_ARROW_REDUCTION);
 	}
 }
